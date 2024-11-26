e92eef42f7e5ef85d348ced4ae8226cc4c68bae4
tossi <tosifraza8867@gmail.com>
2024-11-26 12:29:49 +0530

    login responsive

--- src/components/VisitorRegistration.jsx
+++ src/components/VisitorRegistration.jsx
@@ -206,7 +206,7 @@ const VisitorRegistration = () => {
                       onChange={(e) => setNeedsCab(e.target.checked)}
                     />
                     <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-100 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"></div>
-                    <span className="ms-3 text-sm font-medium text-gray-700">Need cab pickup</span>
+                    <span className="ms-3 text-sm font-medium text-gray-700 sm:inline-block hidden">Need cab pickup</span>
                   </label>  
                 </div>
 
