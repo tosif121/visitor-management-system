a0cac9565b6b6fa6da2081ea7f40b311c68420b3
tossi <tosifraza8867@gmail.com>
2024-11-28 21:20:00 +0530

    ui fixes

---
+++ src/components/ReusableComponents/NoDataFound.jsx
@@ -0,0 +1,14 @@
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+function NoDataFound() {
+  return (
+    <div className="text-center py-16 bg-gray-50 rounded-lg min-h-screen flex justify-center items-center gap-8">
+      <FontAwesomeIcon width={50} height={50} icon={faExclamationTriangle} className="text-6xl text-yellow-500" />
+      <h2 className="text-2xl font-bold text-gray-800 dark:text-white">No Data Found</h2>
+    </div>
+  );
+}
+
+export default NoDataFound;
